c90a57e02b37c1865c8954b9fe067f36e74b172c
mja00 <teamnirvana2@gmail.com>
2025-11-30 17:53:39 -0500

    feat: add dev plugin

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), devtoolsJson()],
   clearScreen: false,
   server: {
     port: 1420,
     strictPort: true,
     host: '127.0.0.1',
-    watch: {
-      ignored: ['**/src-tauri/**'],
-    },
+    watch: { ignored: ['**/src-tauri/**'] }
   },
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
     target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
+    sourcemap: !!process.env.TAURI_DEBUG
+  }
 });
-
